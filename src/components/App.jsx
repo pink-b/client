b0ffc49ee0e46ecbcc82f5740277f8d74bc8f87d
pink-b <60101962+pink-b@users.noreply.github.com>
2024-08-25 21:07:55 +0400

    Add routes and init project

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import AppRoutes from './AppRoutes';
+const App = () => (
+    <div className="container">
+        <AppRoutes />
+    </div>
+)
+
+export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from '../styles/Main.module.css'
+
+const Main = () => {
+    return (
+        <div >
+            Main
+            
+        </div>
+    );
+};
+
+export default Main;
