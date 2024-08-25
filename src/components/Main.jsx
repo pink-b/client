1a204d5f534cafbc67d8211fe0df1c63ba2fead8
pink-b <60101962+pink-b@users.noreply.github.com>
2024-08-25 23:43:38 +0400

    Add sign in inputs

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../styles/Main.module.css'
+import { Link } from 'react-router-dom'
+
+const FIELDS = {
+    NAME: "name",
+    PASSWORD: "password"
+};
 
 const Main = () => {
+    const {NAME, PASSWORD} = FIELDS;
+
+    const [values, setValues] = useState({[NAME]: "", [PASSWORD]: ""});
+
+    const handleChange = ({target: {value, name}}) => {
+        setValues({...values, [name]: value})
+    }
+
+    const handleClick = (e) => {
+        const isDisabled = Object.values(values).some((v) => !v);
+    
+        if (isDisabled) e.preventDefault();
+      };
+    
+
+    console.log(values)
+
     return (
-        <div >
-            Main
+        <div className={styles.wrap}>
+            <div className={styles.container}>
+            <h1 className={styles.heading}>Join</h1>
+            <form className={styles.form}>
+            <div className={styles.group}>
+            <input
+              type="text"
+              name="name"
+              value={values[NAME]}
+              placeholder="Username"
+              className={styles.input}
+              onChange={handleChange}
+              autoComplete="off"
+              required
+            />
+          </div>
+          <div className={styles.group}>
+            <input
+              type="text"
+              name="password"
+              placeholder="Password"
+              value={values[PASSWORD]}
+              className={styles.input}
+              onChange={handleChange}
+              autoComplete="off"
+              required
+            />
+          </div>
+            
+            <Link className={styles.group}
+            onClick={handleClick}
+            to={`/chat?name=${values[NAME]}&room=${values[PASSWORD]}`}>
+                <button type="submit" className={styles.button}>
+                    Sign in
+                </button>
+            </Link>
+            </form>
+            </div>
             
         </div>
     );
